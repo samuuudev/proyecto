38cd58e9764aeef28a9f599183bc1fbacc3614de
Samuel Quintano <samuelmalagonero21@gmail.com>
2025-12-15 03:44:33 +0100

    creacion de la pagina principal para los usuarios normales

---
+++ src/app/components/index/index.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './index.html',
+  styleUrls: ['./index.css']
+})
+export class Index {
+  // Si luego quieres lógica para login, toasts, etc., aquí es donde va
+}
